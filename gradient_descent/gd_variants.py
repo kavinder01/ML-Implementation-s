45d641bcd39a0e47a6e0fcbe3d5ba1d96919c866
Kavinder <kavinder135726@gmail.com>
2026-03-25 11:30:17 +0530

    .

---
+++ gradient_descent/gd_variants.py
@@ -0,0 +1,15 @@
+import numpy as  np
+
+
+X = np.array([[1],[2],[3],[4],[5]], dtype=float)
+y = np.array([1.5, 3.0, 4.5, 6.0, 7.5], dtype=float)
+
+def batch_gradient_descent(X, y, w, b, alpha, iterations):
+
+
+def stochastic_gradient_descent(X, y, w, b, alpha, iterations):
+
+
+
+def mini_batch_gradient_descent(X, y, w, b, alpha, iterations, batch_size=32):
+
